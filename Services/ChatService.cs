2e2023dc6795f059b2a5cc0d762aeea9e323125d
danatunes <57754925+danatunes@users.noreply.github.com>
2021-01-30 20:48:40 +0500

    Add files via upload

---
+++ Services/ChatService.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatProject.Protos;
+
+namespace ChatProject
+{
+
+    public class ChatService : Protos.Chat.ChatBase
+    {
+        private readonly ChatRoom _chatroomService;
+
+        public ChatService(ChatRoom chatRoomService)
+        {
+            _chatroomService = chatRoomService;
+        }
+
+        public override async Task join(IAsyncStreamReader<Message> requestStream, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+        {
+            if (!await requestStream.MoveNext()) return;
+
+            do
+            {
+                _chatroomService.Join(requestStream.Current.User, responseStream);
+                await _chatroomService.BroadcastMessageAsync(requestStream.Current);
+            } while (await requestStream.MoveNext());
+
+            _chatroomService.Remove(context.Peer);
+
+        }
+
+
+    }
+}
